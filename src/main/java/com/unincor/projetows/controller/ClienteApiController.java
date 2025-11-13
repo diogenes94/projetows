59ca3cdcdd46aab98e8f769ff852be56353a463c
diogenes94 <diogenes_afr@hotmail.com>
2025-11-12 21:35:16 -0300

    api

--- src/main/java/com/unincor/projetows/controller/ClienteApiController.java
+++ src/main/java/com/unincor/projetows/controller/ClienteApiController.java
@@ -1,5 +1,28 @@
 package com.unincor.projetows.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unincor.projetows.controller.dto.EnderecoDTO;
+import com.unincor.projetows.model.domain.Cliente;
+import com.unincor.projetows.model.domain.Endereco;
+import com.unincor.projetows.model.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteApiController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping("/{clienteId}/endereco")
+    public EnderecoDTO obterEndereco(@PathVariable Integer clienteId) {
+        Cliente cliente = clienteRepository.findById(clienteId).orElse(null);
+        Endereco e = (cliente != null) ? cliente.getEndereco() : null;
+        return (e == null) ? null : EnderecoDTO.of(e);
+    }
 
 }
